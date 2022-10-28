44bcdd278226630a08ca278e81794fafded6866c
Alvaro García Rodríguez <alvarogaro@correo.ugr.es>
2022-10-28 12:00:24 +0200

    Avanzo #15

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,16 +1,12 @@
 //  Clase Usuario, los cuales van a participar en un evento y van a tener una serie de Gastos y Deudas.
-
-class Usuario{
-    // Lista de Eventos a los que pertenece el usuario
-    // Nombre del usuario (¿Duda si debe ser único o se puede repetir?)
-    // Correo del usuario (¿Duda si debe ser único o se puede repetir?)
-    constructor(Nombre, Correo, Eventos){
-        this.Nombre = Nombre;
-        this.Correo = Correo;
-        this.Eventos = [];
+import Evento from "./Evento";
+class Usuario {
+    /**
+     * 
+     * @param {String} Nombre 
+     */
+    constructor(Nombre) {
+        this.Nombre = Nombre
     }
 }
-
 module.exports.Usuario = Usuario;
-usuario = new Usuario("Usuario1", "alvaroselo@gmail.com",);
-console.log(usuario);
