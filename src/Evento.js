33bb7c870324518ffc24f51ed1614369eb7b53f5
Alvaro García Rodríguez <alvarogaro@correo.ugr.es>
2022-10-28 11:57:21 +0200

    Avance #11 #10

--- src/Evento.js
+++ src/Evento.js
@@ -1,36 +1,19 @@
 // Clase Evento, Representa un Viaje,Evento... que se realiza y que se quiere guardar en la aplicacion.
 // Es la clase Principal sobre la cual va a funcionar la aplicación.
-
-export class Evento{
-    // Fecha del Evento
-    // Nombre del Evento
-    // Array de Participantes (Array de Datos Usuario)
-    // Descripcion del Evento (Opcional (String Con Info Descripción))
-    // Divisa del Evento (Enum con los tipos de Divisa Diponibles (ISO))
-    // Gasto Total del Evento
-    // Pagos del Evento (Array de Datos Pago)
-    // Participantes del Evento (Array de Datos Usuario)
-    // Histórico (¿ Podría realizarse un map con los diferentes participantes y los pagos que han realizado?)
-    // Lista de Pagos con los pagos que ha hecho cada participante.
-    Constructor(Fecha, Nombre, Descripcion, Divisa, Participantes, ){
-        this.Fecha = Fecha;
+import Pago from './Pago.js';
+import Usuario from './Usuario.js';
+export class Evento {
+    /**
+     * 
+     * @param {String} Nombre 
+     * @param {Arraylist(Usuario)} Participantes
+     * @param {Arraylist(Pago)} Pagos
+     *  
+     */
+    Constructor(Nombre, Participantes) {
         this.Nombre = Nombre;
-        this.Descripcion = Descripcion
-        this.Gasto_Total = 0.0;
-        this.Pagos = [];
-        this.Historico = new Map();
         this.Participantes = Participantes;
-        this.Divisa = Divisa;
-    }
-
-    Comprobar_Participante(Usuario){ 
-        // Comprueba si el usuario esta en la lista de participantes
-        for (var i = 0; i < this.Participantes.length; i++) {
-            if (this.Participantes[i] == Usuario){
-                return true;
-            }
-        }
-        return false;   
+        this.Pagos = [];
     }
 
 }
--- src/Pago.js
+++ src/Pago.js
@@ -1,16 +1,15 @@
 //  Clase de Gastos, que van a ir asociados a un Evento y Contienen a un usuario.
 
+import Usuario from './Usuario.js';
 class Pago{
-    // Nombre del Pago
-    // Fecha del Pago (Tipo de Dato Date)
-    // Cantidad a Pagar (Debe de ser un entero positivo.)
-    // Usuario que Paga (Tipo de Dato Usuario)
-    // Usuarios que Participan (Array de Datos Usuario)
-    constructor(Nombre, Fecha, Cantidad, Usuario_paga, Usuarios_participan,){
+    /**
+     * 
+     * @param {Float} Cantidad 
+     * @param {Usuario} Usuario_paga
+     * @constructor Crea un Pago con comprobación de que el saldo introducido sea positivo. 
+     */
+    constructor(Cantidad, Usuario_paga){
         try{
-            this.Nombre = Nombre;
-            // tipeof(Fecha) == Date ? this.Fecha = Fecha : this.Fecha = new Date(Fecha);
-            this.Fecha = Fecha;
             if (Cantidad > 0){
                 this.Cantidad = Cantidad;
             }else{
@@ -18,7 +17,6 @@ class Pago{
                 throw "Cantidad no puede ser negativa";
             }
             this.Usuario_paga = Usuario_paga;
-            this.Usuarios_participan = Usuarios_participan;
         }catch(e){
             console.log(e);
         }
@@ -26,8 +24,4 @@ class Pago{
     }
     
 }
-//  Podemos eliminar el valor de una propiedad mediante la sentencia delete <objeto>.<propiedad>
 module.exports.Pago = Pago;
-//  Realizamos una prueba para ver como funciona la clase Pago
-pagar = new Pago("Pago1", new Date(), 10, "Usuario1", ["Usuario1", "Usuario2"]);
-console.log(pagar);
