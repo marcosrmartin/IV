b425797ecf0d6eec6557fdb1a9778a9188e4abf0
Marcos <75510647+marcosrmartin@users.noreply.github.com>
2022-12-26 03:15:26 +0100

    Ajusto test a la nueva función close #28. Además cambio los datos aleatorios por hechos a mano.

--- src/tests/evento.test.js
+++ src/tests/evento.test.js
@@ -1,6 +1,5 @@
 import { describe, it } from 'node:test';
 import { strict as assert } from 'node:assert';
-import { faker } from '@faker-js/faker';
 
 import Evento from '../services/evento.js'
 import Pago  from '../services/pago.js'
@@ -9,31 +8,38 @@ import Usuario from '../services/usuario.js'
 describe('Evento', () => {
     describe('addPago', () => {
         it('Devuelve el resultado la suma de todos los pagos entre la cantidad de participantes en Evento.Pagos', () => {
-            let usuarios = []
-            let pagos = []
-            let nUsuarios = faker.datatype.number({ max: 6})
-            let nPagos = faker.datatype.number({ max: 30})
-            let total
+            let usuarios = [];
+            let pagos = [];
+            let total;
+            let solucion = [
+                [ 'Bruno', 'Alex', 22.8 ],
+                [ 'Guillermo', 'Alex', 35.8 ],
+                [ 'Tomas', 'Alex', 7.6 ],
+                [ 'Tomas', 'Julia', 28.2 ]
+              ];
 
-            for (let i = 0; i < nUsuarios; i++) {
-                usuarios.push(new Usuario(faker.name.firstName()))
-            }
-            for (let i = 0; i < nPagos; i++) {
-                pagos.push(new Pago(faker.datatype.float({ max: 30 }), usuarios[Math.floor(Math.random()*usuarios.length)]))
-            }
-            let evento = new Evento(faker.address.cityName(), usuarios);
+            usuarios.push(new Usuario("Alex"));
+            usuarios.push(new Usuario("Bruno"));
+            usuarios.push(new Usuario("Guillermo"));
+            usuarios.push(new Usuario("Julia"))
+            usuarios.push(new Usuario("Tomas"))
+            pagos.push(new Pago("Picnic", 13, usuarios[1], usuarios));
+            pagos.push(new Pago("Coche", 64, usuarios[3], usuarios));
+            pagos.push(new Pago("Hotel", 102, usuarios[0], usuarios));
+
+            let evento = new Evento("Viaje a Madrid", usuarios);
             pagos.forEach(pago => {
-                evento.addPago(pago)
+                evento.addPago(pago);
             });
             pagos.forEach(pago => {
-                total += pago.Cantidad
+                total += pago.Cantidad;
             });
             
 
-            let gastoPorUsuario = evento.gastoPorUsuario()
+            let cuentas = evento.getCuentas();
 
-
-            assert.strictEqual(gastoPorUsuario, total/usuarios.length, `No se ha calculado correctamente el resultado ${gastoPorUsuario} != ${total/usuarios.length}`);
+            
+            assert.deepStrictEqual(cuentas, solucion, `No se ha calculado correctamente el resultado {${cuentas}} != {${solucion}}`);
         });
     });
 });
