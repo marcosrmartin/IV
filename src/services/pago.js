7abb56d762fa7009323da720614ffa1f56fd6d01
Marcos <75510647+marcosrmartin@users.noreply.github.com>
2022-12-26 02:59:21 +0100

    Remodelamos Pago resolve #33

--- src/services/pago.js
+++ src/services/pago.js
@@ -6,18 +6,29 @@
 import Usuario from './usuario.js';
 export default class Pago{
     /**
-     * 
+     * @param {String} nombre // Nombre del Pago 
      * @param {Float} cantidad 
      * @param {Usuario} usuarioPaga
+     * @param {Arraylist(Usuario)} participantes
      * @constructor Crea un Pago con comprobación de que el saldo introducido sea positivo. 
      */
-    constructor(cantidad, usuarioPaga){
+    constructor(nombre, cantidad, usuarioPaga, participantes){
         try{
             if (cantidad > 0){
                 this.cantidad = cantidad;
             }else{
                 this.cantidad = 0;
                 throw new Error("Cantidad no puede ser negativa");
+            }
+            if (nombre.length > 0){
+                this.nombre = nombre;
+            }else{
+                throw new Error ("Nombre no puede ser vacío");
+            }
+            if (participantes.length == 0){
+                throw new Error ("Debe haber al menos un participante.");
+            }else{
+                this.participantes = participantes.slice();
             }
             this.usuarioPaga = usuarioPaga;
         }catch(e){
@@ -26,4 +37,19 @@ export default class Pago{
      
     }
     
+    getCantidad(){
+        return this.cantidad;
+    }
+
+    getUsuarioPaga(){
+        return this.usuarioPaga;
+    }
+
+    getParticipantes(){
+        return this.participantes;
+    }
+
+    getNombre(){
+        return this.nombre;
+    }
 }
